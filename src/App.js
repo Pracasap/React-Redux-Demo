bd50643dd5a33e96993141c5d19399c334d833a6
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-07-18 01:10:33 -0500

    mapStateToProps

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import NewCakeContainer from './components/NewCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <NewCakeContainer />
         <IceCreamContainer />
       </div>
