10c1fc17bac3b2c6140ca39b6b1f0c8022d9184f
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-07-18 02:11:25 -0500

    fetch and thunk

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import store from './redux/store';
 import NewCakeContainer from './components/NewCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer cake />
-        <ItemContainer />
-        <NewCakeContainer />
-        <IceCreamContainer />
+        <UserContainer />
       </div>
     </Provider>
   );
